dbc031105948d50506f4fe3d9f70fa6c476fabe2
rocko <ofelix@pm.me>
2022-11-22 16:17:35 +0100

    Disable broken treesitter highlighters

--- modules/home/neovim/plugin/treesitter.lua
+++ modules/home/neovim/plugin/treesitter.lua
@@ -1,10 +1,18 @@
-require'nvim-treesitter.configs'.setup{
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-		-- disable = { "kotlin" },
-	},
-	indent = {
-		enable = true,
-	},
-}
+local hasTreesitter, _ = pcall(require, "nvim-treesitter")
+
+if hasTreesitter then
+    local parser_install_dir = vim.fn.stdpath("cache") .. "/treesitters"
+    vim.fn.mkdir(parser_install_dir, "p")
+    vim.opt.runtimepath:append(parser_install_dir)
+    require'nvim-treesitter.configs'.setup{
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+            disable = { "kotlin", "bash" },
+        },
+        indent = {
+            enable = true,
+        },
+        parser_install_dir = parser_install_dir
+    }
+end
