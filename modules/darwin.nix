dadcebc27e1e617e73af086d10e6e7606c7f0962
rocko <ofelix@pm.me>
2023-06-07 15:53:15 +0200

    loony-tools hash

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -140,6 +140,7 @@ in {
   nix.settings.trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
   ];
   nix.settings.substituters = [
     #    "https://cache.iog.io"
